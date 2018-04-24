0e75a81b95769b1958c760d25df85f50e2645600
evanengelhardt <eaengelh@gmail.com>
2018-04-23 23:22:17 -0500

    Sets up constructor and sigmoid functions

---
+++ network.py
@@ -0,0 +1,19 @@
+import random
+import numpy as np
+
+
+class Network(object):
+
+    def __init__(self, sizes):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y,1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y,x) for x,y in zip(sizes[:-1], sizes[1:])]
+
+    def feedforward(self,a):
+        for b, w in zip(self.biases, self.weights):
+            a = self.sigmoid(np.dot(w, a) + b)
+        return a
+
+    def sigmoid(self, z):
+        return 1/(1+np.exp(-z))
